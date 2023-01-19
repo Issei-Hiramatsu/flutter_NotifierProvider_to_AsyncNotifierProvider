e622870147c93e70f02a527fbf02451a7cf8ef30
issei <issei.hiramatsu@gmail.com>
2023-01-19 15:58:10 +0900

    Fix error Migrate to AsyncNotifier

--- lib/view/auth_and_todoPage.dart
+++ lib/view/auth_and_todoPage.dart
@@ -4,6 +4,7 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import '../domain/item_model/item_model.dart';
 import '../extensions/custom_exception.dart';
 import '../use_case/auth_controller.dart';
+import '../use_case/item_list_controller.dart';
 
 class HomeScreen extends HookConsumerWidget {
   @override
